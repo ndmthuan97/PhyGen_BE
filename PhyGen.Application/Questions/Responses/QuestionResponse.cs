86ee9a269fbe33af88bc466f94e2e2101092271e
TheCollector009 <hoangxd3549@gmail.com>
2025-07-03 16:46:40 +0700

    [Fix] Response Type & Level in Question & Matrix Section Detail

--- PhyGen.Application/Questions/Responses/QuestionResponse.cs
+++ PhyGen.Application/Questions/Responses/QuestionResponse.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace PhyGen.Application.Questions.Responses
@@ -12,8 +13,12 @@ namespace PhyGen.Application.Questions.Responses
         public Guid Id { get; set; }
         public Guid TopicId { get; set; }
         public string Content { get; set; } = string.Empty;
+        [JsonIgnore]
         public DifficultyLevel Level { get; set; }
+        public string LevelName => Level.ToString();    
+        [JsonIgnore]
         public QuestionType Type { get; set; }
+        public string TypeName => Type.ToString();
         public string? Image { get; set; }
     }
 }
