62e3bb8631b1e9a8c5def18b55ced2f7cd96d6de
TheCollector009 <hoangxd3549@gmail.com>
2025-08-05 12:15:46 +0700

    [Add] API Update Matrix Full

---
+++ PhyGen.Application/Matrices/Commands/UpdateMatrixFullCommand.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using PhyGen.Shared.Constants;
+
+namespace PhyGen.Application.Matrices.Commands
+{
+    public class UpdateMatrixFullCommand : IRequest<Unit>
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public int TotalQuestionCount { get; set; }
+        public int Grade { get; set; }
+        public int Year { get; set; }
+        public string? ImgUrl { get; set; }
+
+        public List<MatrixSectionDto> Sections { get; set; } = new();
+    }
+
+    public class MatrixSectionDto
+    {
+        public Guid? Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public double? Score { get; set; }
+        public string? Description { get; set; }
+        public List<MatrixSectionDetailDto> Details { get; set; } = new();
+    }
+
+    public class MatrixSectionDetailDto
+    {
+        public Guid? Id { get; set; }
+        public Guid SectionId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public DifficultyLevel Level { get; set; }
+        public QuestionType Type { get; set; }
+        public int Quantity { get; set; }
+    }
+}
