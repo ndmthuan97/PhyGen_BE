296c7e8b4d8fd1a742025beb3524711e41efccee
thinhntg <nguyenthinh23032003ha@gmail.com>
2025-08-17 21:56:25 +0700

    [Update] API Transaction

--- PhyGen.Application/PayOs/Response/SearchTransactionResponse.cs
+++ PhyGen.Application/PayOs/Response/SearchTransactionResponse.cs
@@ -10,6 +10,7 @@ namespace PhyGen.Application.PayOs.Response
     public class SearchTransactionResponse
     {
         public int CoinChange { get; set; }
+        public int CoinBefore { get; set; }
         public int CoinAfter { get; set; }
         public DateTime CreatedAt { get; set; }
         public string Type { get; set; }
